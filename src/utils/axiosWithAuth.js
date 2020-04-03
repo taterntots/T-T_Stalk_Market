b9204b41cad21034daf4df6b0300531356d4f61d
taterntots <matthew.a.weidner@gmail.com>
2020-04-03 13:00:09 -0400

    changed the baseURL in my axiosWithAuth to the heroku backend link

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
   return axios.create({
-    baseURL: process.env.REACT_APP_databaseURL,
+    baseURL: 'https://tnt-stalk-market-be-production.herokuapp.com/api',
     headers: {
       authorization: token
     }
